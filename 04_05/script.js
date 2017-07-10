67d2c96ecb5c4ecc6f90082872becc09bba1b2f1
Kevin Pyles <kevinpyles@gmail.com>
2017-07-10 15:05:27 -0600

    Complete through 04_05

---
+++ 04_05/script.js
@@ -0,0 +1,11 @@
+var a = 5/7;
+var b = 18/25;
+
+var theBiggest = function(a,b){
+  var result;
+  a>b ? result = ["a", a] : result = ["b", b];
+  return result;
+}
+
+console.log(theBiggest(a,b));
+console.log(theBiggest(7/9,13/25));
