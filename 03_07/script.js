75ba85dd3e2e26c5886abae00bfe959b32aa3f3a
Kevin Pyles <kevinpyles@gmail.com>
2017-07-10 14:00:00 -0600

    Updated through Exercise 03_08

---
+++ 03_07/script.js
@@ -0,0 +1,10 @@
+var pens;
+
+pens = ["red", "blue", "green", "orange"];
+
+var mixedBag = ["keys", 5, false, 568, "one more thing"];
+
+var fourthPen = pens[3];
+
+console.log(pens);
+console.log(fourthPen);
